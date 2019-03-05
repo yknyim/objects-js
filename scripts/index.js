a1eb0009635ef6a10759ed4afd72139c898c3033
Ryan Yim <yknyim@gmail.com>
2019-03-05 10:50:25 -0500

    adds leet dictionary to object index.js

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+console.log('hello?')
+
+const leetDictionary = {
+    // Keys are strings, just like in python
+    // But in JS, quotes are optional
+    A: 4,
+    E: 3,
+    G: 6,
+    I: 1,
+    O: 0,
+    S: 5,
+    T: 7,
+};
